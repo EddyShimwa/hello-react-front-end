c12e4dc112d31920ffea41d5c51bfa2c7addfd25
EddyShimwa <shedpray16@gmail.com>
2023-05-12 09:04:46 +0200

    set up the git flow

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,1 @@
-import { configureStore } from '@reduxjs/toolkit';
-import messagesReducer from '../reducers/messagesSlice';
-
-const store = configureStore({
-  reducer: {
-    messages: messagesReducer,
-  },
-});
-
-export default store;
+// #for configurestore

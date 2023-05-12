68e2f781b042773336f281160862943e6886442c
EddyShimwa <shedpray16@gmail.com>
2023-05-12 09:06:43 +0200

    added the reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,1 +1,10 @@
-// #for configurestore
+import { configureStore } from '@reduxjs/toolkit';
+import messagesReducer from '../reducers/messagesSlice';
+
+const store = configureStore({
+  reducer: {
+    messages: messagesReducer,
+  },
+});
+
+export default store;
